51d46d47d3fc43d50a17db89c453ab44e7e64f0d
Milan <mc@radiopiloten.de>
2025-03-20 23:16:20 +0100

    Log: shorten

--- lib/StreamSchedule/Log.pm
+++ lib/StreamSchedule/Log.pm
@@ -6,34 +6,26 @@ our @EXPORT_OK = qw(printInfo printWarning printError printExit) ;
 our %EXPORT_TAGS = ( 'all'  => [ @EXPORT_OK ] );
 
 sub printInfo{
-    my $message=shift;
+    my ($message) = @_;
     chomp $message;
-    #my ($package, $filename, $line, $subroutine) = caller(1);
-    #$message="$filename, line $line, $package::$subroutine: ".$message;
     print STDERR "INFO: $message\n";
 }
 
 sub printWarning{
-    my $message=shift;
+    my ($message) = @_;
     chomp $message;
-    #my ($package, $filename, $line, $subroutine) = caller(1);
-    #$message="$filename, line $line, $package::$subroutine: ".$message;
     print STDERR "WARNING: $message\n";
 }
 
 sub printError{
-	my $message=shift;
-	chomp $message;
-    #my ($package, $filename, $line, $subroutine) = caller(1);
-    #$message="$filename, line $line, $package::$subroutine: ".$message;
-	print STDERR "ERROR: $message\n" ;
+    my ($message) = @_;
+    chomp $message;
+    print STDERR "ERROR: $message\n" ;
 }
 
 sub printExit{
-    my $message=shift;
+    my ($message) = @_;
     chomp $message;
-    #my ($package, $filename, $line, $subroutine) = caller(1);
-    #$message="$filename, line $line, $package::$subroutine: ".$message;
     print STDERR "FATAL: $message\n" ;
     exit 1;
 }
